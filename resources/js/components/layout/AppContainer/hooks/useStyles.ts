714695c3dec6bd5abe57f4704ff3b9bd9084f5e5
Sam <samuelharvey15@hotmail.com>
2022-08-21 01:36:32 +0100

    appbar improvements

--- resources/js/components/layout/AppContainer/hooks/useStyles.ts
+++ resources/js/components/layout/AppContainer/hooks/useStyles.ts
@@ -4,13 +4,13 @@ export const useStyles = () => {
     return useMakeStyles({
         appBar: {
             backgroundColor: 'white',
+            boxShadow: '0px 0px 15px 9px rgba(0,0,0,0.1)',
         },
 
         bannerImage: {
             height: '100px',
             width: '280px',
             opacity: 0.4,
-            borderRight: '2px solid black',
         },
     });
 };
